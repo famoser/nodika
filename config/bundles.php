572ececf04e7d410ffa12cb5ac7e63ae8e083265
Florian Moser <git@famoser.ch>
2024-07-29 18:12:00 +0200

    maint: Remove more superfluous tools

--- config/bundles.php
+++ config/bundles.php
@@ -7,6 +7,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
