5a6a961ceed0c9ee84b8e37ef54a8c7ad074516f
Florian Moser <git@famoser.ch>
2024-07-29 18:35:44 +0200

    maint: Update php cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,15 +9,16 @@ For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 COMMENT;
 
-$finder = PhpCsFixer\Finder::create()
-    ->in('src')
-    ->in('tests')
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
 ;
 
-$config = new PhpCsFixer\Config();
-return $config->setRules([
-    '@Symfony' => true,
-    'array_syntax' => ['syntax' => 'short'],
-    'header_comment' => ['header' => $fileHeaderComment, 'separate' => 'both'],
-])
-    ->setFinder($finder);
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'header_comment' => ['header' => $fileHeaderComment, 'separate' => 'both'],
+    ])
+    ->setFinder($finder)
+;
