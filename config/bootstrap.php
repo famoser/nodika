84ff025f4d8c095d8dfa09da3643958895ed2ba8
Florian Moser <git@famoser.ch>
2024-07-29 18:36:07 +0200

    maint: Update code style

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,8 +1,17 @@
 <?php
 
+/*
+ * This file is part of the nodika project.
+ *
+ * (c) Florian Moser <git@famoser.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
 if (!array_key_exists('APP_ENV', $_SERVER)) {
     $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] ?? null;
@@ -13,11 +22,11 @@ if ('prod' !== $_SERVER['APP_ENV']) {
         throw new RuntimeException('The "APP_ENV" environment variable is not set to "prod". Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
     }
 
-    $path = dirname(__DIR__) . '/.env';
+    $path = dirname(__DIR__).'/.env';
     $dotenv = new Dotenv(false);
     $dotenv->loadEnv($path);
 }
 
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?: $_ENV['APP_ENV'] ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
-$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int)$_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
