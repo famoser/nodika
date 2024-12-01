fd7bb71b7c5464c9526acf18e94b61d9773dc859
Florian Moser <git@famoser.ch>
2024-12-01 16:32:12 +0100

    maint: Update doctrine migrations

--- config/bundles.php
+++ config/bundles.php
@@ -2,8 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
@@ -11,4 +9,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
---
+++ migrations/Version20241201153104.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241201153104 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE migration_versions');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__doctor AS SELECT id, is_administrator, receives_administrator_mail, email, password_hash, reset_hash, is_enabled, registration_date, last_login_date, invitation_identifier, last_invitation, job_title, given_name, family_name, street, street_nr, address_line, postal_code, city, country, phone, deleted_at FROM doctor');
+        $this->addSql('DROP TABLE doctor');
+        $this->addSql('CREATE TABLE doctor (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, is_administrator BOOLEAN NOT NULL, receives_administrator_mail BOOLEAN NOT NULL, email CLOB NOT NULL, password_hash CLOB NOT NULL, reset_hash CLOB NOT NULL, is_enabled BOOLEAN NOT NULL, registration_date DATETIME DEFAULT NULL, last_login_date DATETIME DEFAULT NULL, invitation_identifier CLOB DEFAULT NULL, last_invitation DATETIME DEFAULT NULL, job_title CLOB DEFAULT NULL, given_name CLOB NOT NULL, family_name CLOB NOT NULL, street CLOB DEFAULT NULL, street_nr CLOB DEFAULT NULL, address_line CLOB DEFAULT NULL, postal_code INTEGER DEFAULT NULL, city CLOB DEFAULT NULL, country CLOB DEFAULT NULL, phone CLOB DEFAULT NULL, deleted_at DATETIME DEFAULT NULL)');
+        $this->addSql('INSERT INTO doctor (id, is_administrator, receives_administrator_mail, email, password_hash, reset_hash, is_enabled, registration_date, last_login_date, invitation_identifier, last_invitation, job_title, given_name, family_name, street, street_nr, address_line, postal_code, city, country, phone, deleted_at) SELECT id, is_administrator, receives_administrator_mail, email, password_hash, reset_hash, is_enabled, registration_date, last_login_date, invitation_identifier, last_invitation, job_title, given_name, family_name, street, street_nr, address_line, postal_code, city, country, phone, deleted_at FROM __temp__doctor');
+        $this->addSql('DROP TABLE __temp__doctor');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE migration_versions (version VARCHAR(191) NOT NULL COLLATE "BINARY", executed_at DATETIME DEFAULT NULL, execution_time INTEGER DEFAULT NULL, PRIMARY KEY(version))');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__doctor AS SELECT id, is_administrator, receives_administrator_mail, street, street_nr, address_line, postal_code, city, country, phone, email, invitation_identifier, last_invitation, job_title, given_name, family_name, deleted_at, password_hash, reset_hash, is_enabled, registration_date, last_login_date FROM doctor');
+        $this->addSql('DROP TABLE doctor');
+        $this->addSql('CREATE TABLE doctor (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, is_administrator BOOLEAN NOT NULL, receives_administrator_mail BOOLEAN NOT NULL, street CLOB DEFAULT NULL, street_nr CLOB DEFAULT NULL, address_line CLOB DEFAULT NULL, postal_code INTEGER DEFAULT NULL, city CLOB DEFAULT NULL, country CLOB DEFAULT NULL, phone CLOB DEFAULT NULL, email CLOB NOT NULL, invitation_identifier CLOB DEFAULT NULL, last_invitation DATETIME DEFAULT NULL, job_title CLOB DEFAULT NULL, given_name CLOB NOT NULL, family_name CLOB NOT NULL, deleted_at DATETIME DEFAULT NULL, password_hash CLOB NOT NULL, reset_hash CLOB NOT NULL, is_enabled BOOLEAN NOT NULL, registration_date DATETIME DEFAULT NULL, last_login_date DATETIME DEFAULT NULL)');
+        $this->addSql('INSERT INTO doctor (id, is_administrator, receives_administrator_mail, street, street_nr, address_line, postal_code, city, country, phone, email, invitation_identifier, last_invitation, job_title, given_name, family_name, deleted_at, password_hash, reset_hash, is_enabled, registration_date, last_login_date) SELECT id, is_administrator, receives_administrator_mail, street, street_nr, address_line, postal_code, city, country, phone, email, invitation_identifier, last_invitation, job_title, given_name, family_name, deleted_at, password_hash, reset_hash, is_enabled, registration_date, last_login_date FROM __temp__doctor');
+        $this->addSql('DROP TABLE __temp__doctor');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1FC0F36AE7927C74 ON doctor (email)');
+    }
+}
